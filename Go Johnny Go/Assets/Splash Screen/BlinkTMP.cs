b2883bfa2ca964f865b18230e49f813915516823
JeetChakrabarty <jeet.chakrabarty@gmail.com>
2018-03-07 15:16:20 -0500

    Updated Some Assets
    Created Splash Screen

---
+++ Go Johnny Go/Assets/Splash Screen/BlinkTMP.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class BlinkTMP : MonoBehaviour {
+
+	public TMP_Text text;
+	public float last, current;
+	public bool state;
+
+	void Start()
+	{
+		state = true;
+		last = Time.time;
+		text = GetComponent<TMP_Text> ();
+		text.color = Color.white;
+	}
+
+	void Update()
+	{
+		current = Time.time;
+		if (current > last + 0.4) {
+			state = !state;
+			last = current;
+			if (text.color == Color.white)
+				text.color = Color.clear;
+			else
+				text.color = Color.white;
+		}
+	}
+}
