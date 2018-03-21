56bfff94bdc9d5e8fb05eb35fe61d12c1929cf00
John Abed <john.abed@hotmail.com>
2018-03-21 00:47:59 -0400

    Shop screen completed with one purchasable guitar. Need to make back button work and add the link from main menu to reach screen. Need to make playerprefs still to track coins.

---
+++ Go Johnny Go/Assets/Shop Screen/BuyButton.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuyButton : MonoBehaviour {
+
+	public Text coinsText;
+	public Button buyButton;
+	public Text amountText;
+	public Text buyText;
+	public int coins;
+	public int price;
+
+	// Use this for initialization
+	void Start () {
+		Button btn = buyButton.GetComponent<Button>();
+		btn.onClick.AddListener(BuyOnClick);
+
+		coins = int.Parse(coinsText.text); //should be in playerprefs
+		int index = amountText.text.IndexOf (" "); //used to remove " Coins"
+		price = int.Parse(amountText.text.Substring(0,index));
+	}
+
+	// Update is called once per frame
+	void Update () {
+		//makes buy button clickable if funds available
+		if (price <= coins) {
+			buyButton.interactable = true;
+		}
+		else {
+			buyButton.interactable = false;	
+		}
+	}
+
+	void BuyOnClick()
+	{		
+			coins -= price;
+			coinsText.text = coins.ToString();
+			amountText.text = "";
+			buyText.text = "OWNED";
+			buyButton.image.enabled = false;
+	}
+}
