fac5cbf8b0eaa1d4652e14564aa62082a47f06ee
Lankesh Patel <lpatel7@uwo.ca>
2018-03-22 12:53:09 -0400

    Pushing checkpoint feature

--- Go Johnny Go/Assets/Scripts/Checkpoint.cs
+++ Go Johnny Go/Assets/Scripts/Checkpoint.cs
@@ -1,16 +1,35 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+namespace UnityStandardAssets._2D
+{
+    public class Checkpoint : MonoBehaviour
+    {
 
-public class Checkpoint : MonoBehaviour {
+        public LevelManager levelManager;
+        SpriteRenderer sr;
+        public Sprite newSprite;
+        // Use this for initialization
+        void Start()
+        {
+            levelManager = FindObjectOfType<LevelManager>();
+            sr = GetComponent<SpriteRenderer>();
+        }
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        void OnTriggerEnter2D(Collider2D other)
+        {
+            if (other.name == "Player")
+            {
+                sr.sprite = newSprite;
+                levelManager.currentCheckpoint = gameObject;
+                GetComponent<CircleCollider2D>().enabled = false;
+            }
+        }
+    }
 }
