36d2e09f62ae346aa0900893836369d97024a72e
AidenM <aidenmllr@gmail.com>
2018-03-24 21:41:54 -0400

    Added music, boss level

---
+++ Go Johnny Go/Assets/GameMusic.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameMusic : MonoBehaviour {
+
+// kill menu music
+
+	// Use this for initialization
+	void Awake()
+	{
+		GameObject[] objs = GameObject.FindGameObjectsWithTag ("music");
+
+		Destroy (objs[0]);
+
+	}
+}
