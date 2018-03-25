31a19dd8c89d114f3f13550c550e480f9d937fa6
John Abed <john.abed@hotmail.com>
2018-03-25 15:47:29 -0400

    Working equip functionality

--- Go Johnny Go/Assets/Shop Screen/ShopControlScript.cs
+++ Go Johnny Go/Assets/Shop Screen/ShopControlScript.cs
@@ -14,10 +14,10 @@ public class ShopControlScript : MonoBehaviour {
 	public Text buyText;
 	public int guitar_b_price;
 	public Button backButton;
-	/*public Button equipButton1;
+	public Button equipButton1;
 	public Button equipButton2;
 	public Text equipText1;
-	public Text equipText2;*/
+	public Text equipText2;
 
 
 	// Use this for initialization
@@ -27,14 +27,15 @@ public class ShopControlScript : MonoBehaviour {
 		moneyAmount = PlayerPrefs.GetInt("MoneyAmount");
 		guitarBought = PlayerPrefs.GetInt ("GuitarB_Bought") == 1 ? true : false;
 		setBought (guitarBought);
-		//changeGuitar (PlayerPrefs.GetInt ("GuitarType"));
 
 		coinsText.text = moneyAmount.ToString ();
-		Button btn = buyButton.GetComponent<Button>();
-		btn.onClick.AddListener(BuyOnClick);
 
-		Button btn2 = backButton.GetComponent<Button>();
-		btn2.onClick.AddListener(mainMenuScene);
+		buyButton.onClick.AddListener(BuyOnClick);
+		backButton.onClick.AddListener(mainMenuScene);
+		equipButton1.onClick.AddListener(delegate{changeGuitar(0);});
+		equipButton2.onClick.AddListener(delegate{changeGuitar(1);});
+
+		changeGuitar (PlayerPrefs.GetInt ("GuitarType"));
 	}
 
 	// Update is called once per frame
@@ -52,7 +53,7 @@ public class ShopControlScript : MonoBehaviour {
 			moneyAmount -= guitar_b_price;
 			PlayerPrefs.SetInt("MoneyAmount", moneyAmount);
 			PlayerPrefs.SetInt("GuitarB_Bought", 1); //indicates that 2nd guitar has been purchased
-			//changeGuitar(1);	
+			changeGuitar(1);	
 			coinsText.text = moneyAmount.ToString();
 			setBought (true);
 	}
@@ -74,12 +75,24 @@ public class ShopControlScript : MonoBehaviour {
 		SceneManager.LoadScene ("Main Menu");
 	}
 
-	/*
+
 	void changeGuitar(int guitarNum) {
 		PlayerPrefs.SetInt("GuitarType", guitarNum);
-		equipButton1.enabled = guitarNum == 0 ? false : true;
-		equipText1.enabled = guitarNum == 0 ? true : false;
-		equipButton2.enabled = guitarNum == 0 ? true : false;
-		equipText2.enabled = guitarNum == 0 ? false : true;
-	}*/
+		if (guitarNum == 0) {
+			equipButton1.gameObject.SetActive (false);
+			equipText1.enabled = true;
+			equipButton2.gameObject.SetActive (true);
+			equipText2.enabled = false;
+		} else {
+			equipButton1.gameObject.SetActive (true);
+			equipText1.enabled = false;
+			equipButton2.gameObject.SetActive (false);
+			equipText2.enabled = true;
+		}
+
+		if (!guitarBought) {
+			equipButton2.gameObject.SetActive (false);
+			equipText2.enabled = false;
+		}
+	}
 }
