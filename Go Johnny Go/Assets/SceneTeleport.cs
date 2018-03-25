989def6205149915f0fc88f2d1ae457e5f6b0ea4
AidenM <aidenmllr@gmail.com>
2018-03-25 00:48:45 -0400

    added level transition to boss level

---
+++ Go Johnny Go/Assets/SceneTeleport.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneTeleport : MonoBehaviour {
+
+	void OnTriggerEnter2D(Collider2D other){
+
+		if(other.tag == "Player")
+			//SceneManager.LoadScene ("boss_level", LoadSceneMode.Single);
+			Debug.Log("hello");
+	}
+
+}
