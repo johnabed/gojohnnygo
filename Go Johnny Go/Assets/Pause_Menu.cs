75e5947cb9d970f0d343fc27da392c058515e824
John Abed <john.abed@hotmail.com>
2018-03-27 15:59:05 -0400

    Pause Menu mutes the song playing. Game over menu keeps it playing still though.

--- Go Johnny Go/Assets/Pause_Menu.cs
+++ Go Johnny Go/Assets/Pause_Menu.cs
@@ -8,16 +8,25 @@ public class Pause_Menu : MonoBehaviour {
     public GameObject pauseMenuUI;
     string menu = "Main Menu";
 
+	public AudioSource songSrc;
+	public AudioClip songClip;
+
+	void Start () {
+		songSrc.clip = songClip;
+	}
+
 	// Update is called once per frame
 	void Update () {
 		if (Input.GetKeyDown(KeyCode.P) || Input.GetKeyDown(KeyCode.Escape))
         {
             if (GameIsPaused)
             {
+				songSrc.UnPause ();
                 Resume();
             }
             else
             {
+				songSrc.Pause ();
                 Pause();
             }
         }
