f7706e118aeaa9cf2f613b281a5579b1fc034f55
John Abed <john.abed@hotmail.com>
2018-03-28 02:43:06 -0400

    Boss health bar and cage falls when depleted.

---
+++ Go Johnny Go/Assets/Scripts/CageChainsBehaviour.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CageChainsBehaviour : MonoBehaviour {
+
+	[SerializeField]
+	private Image bossHealthbar;
+	private int bossHealth;
+	private int bossMaxHealth;
+
+	[SerializeField]
+	private GameObject cage;
+	[SerializeField]
+	private GameObject chains;
+
+	void Start() {
+		bossHealth = 50;
+		bossMaxHealth = 50;
+	}
+
+	void OnTriggerEnter2D(Collider2D other){
+		PlayerControl playerScript = FindObjectOfType<PlayerControl> ();
+
+		if (other.tag == "Note" && bossHealth > 0) {
+			bossHealth -= playerScript.guitarDmg;
+			bossHealthbar.fillAmount = (float)bossHealth / (float)bossMaxHealth;
+			if (bossHealth <= 0) {
+				Debug.Log ("GAME WON");
+				Destroy (chains);
+			}
+			Destroy (other);
+		}
+	}
+
+}
